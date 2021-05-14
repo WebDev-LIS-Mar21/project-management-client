88fee66757cd54e44ece0d9a1eba2c54d46a2b88
WebDev-LIS-Mar21 <81487104+WebDev-LIS-Mar21@users.noreply.github.com>
2021-05-14 09:15:11 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import "./App.css";
+import React from "react";
+import ListProjects from "./components/ListProjects";
+import { Switch, Route } from "react-router-dom";
+import ProjectDetails from "./components/ProjectDetails";
+import AddProject from "./components/AddProject";
+import Navbar from "./components/Navbar";
+import EditProject from "./components/EditProject";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { loggedin } from "./api";
+
+class App extends React.Component {
+  state = {
+    loggedInUser: null,
+  };
+
+  async componentDidMount() {
+    if (this.state.loggedInUser === null) {
+      const response = await loggedin();
+      if (response.data._id) {
+        this.setCurrentUser(response.data);
+      }
+    }
+  }
+
+  setCurrentUser = (user) => {
+    this.setState({
+      loggedInUser: user,
+    });
+  };
+
+  render() {
+    const { loggedInUser } = this.state;
+    return (
+      <div className="App">
+        <Navbar
+          loggedInUser={loggedInUser}
+          setCurrentUser={this.setCurrentUser}
+        />
+        <Switch>
+          <Route exact path={["/", "/projects"]} component={ListProjects} />
+          <Route exact path="/projects/add" component={AddProject} />
+          <Route exact path="/projects/:id" component={ProjectDetails} />
+          <Route exact path="/projects/:id/edit" component={EditProject} />
+          <Route exact path="/signup" component={Signup} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => {
+              return <Login {...props} setCurrentUser={this.setCurrentUser} />;
+            }}
+          />
+          <Route
+            exact
+            path="/login-google"
+            render={() => {
+              window.location.href = `${process.env.REACT_APP_PROJECTS_API}/api/auth/google`;
+            }}
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
